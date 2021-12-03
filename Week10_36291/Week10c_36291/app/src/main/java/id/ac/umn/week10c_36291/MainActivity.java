d76653be56449c60b98f527ab89890a091a941b7
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2021-12-03 11:27:50 +0700

    commit week 10c

---
+++ Week10_36291/Week10c_36291/app/src/main/java/id/ac/umn/week10c_36291/MainActivity.java
@@ -0,0 +1,29 @@
+package id.ac.umn.week10c_36291;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent servIntent = new Intent(this, SimpleIntentService.class);
+        startService(servIntent);
+        Button btnStartService = findViewById(R.id.main_button_startservice);
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        CustomService.class);
+                startService(intent);
+            }
+        });
+    }
+
+}
